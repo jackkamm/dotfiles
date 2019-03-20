d7e21d4bd935f601ac0bf63b17b61143545a25c2
Jack Kamm <jackkamm@gmail.com>
2019-03-19 23:16:39 -0700

    Rename emacs_daemon.sh, and launch emacs with systemctl

--- scripts/emacs_daemon.sh
+++ scripts/emacs_daemon.sh
@@ -1,3 +1,4 @@
+systemctl --user enable --now emacs.service
 stow -R -t ~ -d stow emacsclient-desktop
 xdg-mime default emacsclient.desktop text/plain
 xdg-mime default emacsclient-mail.desktop x-scheme-handler/mailto
