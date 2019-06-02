5d577add29c2c34316cc143e6de090929ccc8ce3
Jack Kamm <jackkamm@gmail.com>
2019-06-02 14:44:30 -0700

    Replace emacsclient.desktop with emacs.desktop

--- scripts/emacs_daemon.sh
+++ scripts/emacs_daemon.sh
@@ -1,4 +1,4 @@
 systemctl --user enable --now emacs.service
 stow -R -t ~ -d stow emacsclient-desktop
-xdg-mime default emacsclient.desktop text/plain
+xdg-mime default emacs.desktop text/plain
 xdg-mime default emacsclient-mail.desktop x-scheme-handler/mailto
