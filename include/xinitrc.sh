8fd1fb011ad0111f485327200f1219c624138439
Jack Kamm <jackkamm@gmail.com>
2019-10-16 23:12:15 -0700

    Use GTK theme in QT apps

--- include/xinitrc.sh
+++ include/xinitrc.sh
@@ -7,6 +7,10 @@ dbus-update-activation-environment --systemd DISPLAY
 eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
 export SSH_AUTH_SOCK
 
+# https://wiki.archlinux.org/index.php/Uniform_look_for_Qt_and_GTK_applications
+# requires package qt5-styleplugins
+export QT_QPA_PLATFORMTHEME=gtk2
+
 ## increase trackpoint acceleration speed
 #xinput set-prop "TPPS/2 IBM TrackPoint" 299 1
 
