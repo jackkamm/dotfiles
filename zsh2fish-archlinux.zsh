e9600fe71353571fabe25c4dc4775ed4f1e29421
Jack Kamm <jackkamm@gmail.com>
2020-02-04 14:43:53 -0800

    zsh: disable autosuggest

--- zsh2fish-archlinux.zsh
+++ zsh2fish-archlinux.zsh
@@ -1,7 +1,7 @@
 # Source this from ~/.zshrc like:
 # [[ -r ~/src/dotfiles/include/zsh2fish-archlinux.zsh ]] && . ~/src/dotfiles/include/zsh2fish-archlinux.zsh
 
-source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+#source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE=""
 
 source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
