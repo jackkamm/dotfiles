a6ac1aaf018623c20da9ca8890bc99164bd569cf
Jack Kamm <jackkamm@gmail.com>
2021-06-05 14:10:12 -0700

    Update emacs-daemon-setup.sh

--- emacs-daemon-setup.sh
+++ emacs-daemon-setup.sh
@@ -1,4 +1,4 @@
 systemctl --user enable --now emacs.service
-stow -R -t ~ -d stow emacsclient-desktop
+stow -R emacsclient-desktop
 xdg-mime default emacs.desktop text/plain
 xdg-mime default emacsclient-mail.desktop x-scheme-handler/mailto
