bb40199b79b1e480c70a7f30a92e1d374a722676
Jack Kamm <jackkamm@gmail.com>
2025-10-21 19:12:31 -0700

    setup-shell: Make it more robust
    Only append to file if it exists. In particular I had problems on
    debian due to creating ~/.bash_profile which prevented ~/.profile from
    being sourced
    
    Also add to ~/.profile for distros (like debian) that prefer that over
    ~/.bash_profile
    
    Also allow the script to be run multiple times without adding the line
    if already previously added

--- setup-shell.bash
+++ setup-shell.bash
@@ -1,18 +1,24 @@
 #!/bin/bash
 set -euo pipefail
 {
+    # adds a line to source $1 in the rc-file $2 if $2 exists and
+    # doesn't contain $1.
     print_include () {
-        echo -e "\n[[ -r $1 ]] && . $1"
+        if [ -f $2 ] && ! grep -qF $1 $2 ; then
+            echo -e "\n[[ -r $1 ]] && . $1" >> $2
+            echo "Appended to $2"
+        fi
     }
-
-    print_include ~/.config/include/profile.sh >> ~/.bash_profile
-    print_include ~/.config/include/profile.sh >> ~/.zprofile
-    print_include ~/.config/include/bashrc.bash >> ~/.bashrc
-    print_include ~/.config/include/zshrc.zsh >> ~/.zshrc
-    echo "Appended to .bash_profile, .zprofile, .bashrc, .zshrc."
 
     ./stow-home.bash common
     echo "Stowed common dotfiles."
+
+    print_include ~/.config/include/profile.sh ~/.profile
+    print_include ~/.config/include/profile.sh ~/.bash_profile
+    print_include ~/.config/include/profile.sh ~/.zprofile
+
+    print_include ~/.config/include/bashrc.bash ~/.bashrc
+    print_include ~/.config/include/zshrc.zsh ~/.zshrc
 
     echo "Done."
     exit
